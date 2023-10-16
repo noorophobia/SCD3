3b8a5e2e36d6dfb24ceae36f4a6a4e027d3b0f70
Noor Fatima <102344050+noorophobia@users.noreply.github.com>
2023-10-17 00:21:12 +0500

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Assignment3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
